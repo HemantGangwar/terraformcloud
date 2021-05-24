c8c9998182431763e77feea137b8165e0739bd5d
Hemant Gangwar <38517925+HemantGangwar@users.noreply.github.com>
2021-05-24 20:40:00 +0530

    Update ec2.tf
    Df

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "r100c96" {
   availability_zone = "ap-south-1b"
   key_name      = "aws-exam-testing"
   user_data = file("installcommand.sh")
-  count = 0
+  count = 5
   tags = {
     Name  = "Terraform-diff-linux-${count.index}"
   }
